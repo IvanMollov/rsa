b368e01268b94a52c677315c84c96addd1b0af1d
IvanMollov <ivan.xero.mollov@gmail.com>
2018-06-10 18:51:51 +0300

    working, basic interface

--- frequency-table.hpp
+++ frequency-table.hpp
@@ -7,18 +7,24 @@
 #include <vector>
 
 #include <thread>
+#include <mutex>
 
 #include <fstream>
+#include <iostream>
 
 #include <cstddef>
 #include <cmath>
 
 #include <cassert>
 
+#include <chrono>
+
 namespace FrequencyTable
 {
+	std::mutex _mutex;
+
 	static const std::size_t FREQUENCY_TABLE_SIZE = 256;
-	using Type = std::array<unsigned long long, FREQUENCY_TABLE_SIZE + 1>;
+	using Type = std::array<unsigned long long, FREQUENCY_TABLE_SIZE>;
 
 	void count_symbols(char* buffer, std::size_t buffer_size, Type& frequencies)
 	{
@@ -26,7 +32,7 @@ namespace FrequencyTable
 			const unsigned char symbol_index = static_cast<const unsigned char>(buffer[i]);
 
 			frequencies[symbol_index] += 1;
-			frequencies[256] += 1; // count processed bytes
+			//frequencies[256] += 1; // count processed bytes // to use, resize frequencies to 257
 		}
 	}
 
@@ -69,17 +75,19 @@ namespace FrequencyTable
 		return merged_frequencies;
 	}
 
-	inline Type get(const std::string& file_path, unsigned int threads_count)
+	inline Type get(const std::string& file_path, unsigned int threads_count, std::ostream& log_stream = std::cout, bool is_quiet = true)
 	{
 		std::streampos file_size = Utils::File::get_file_size(file_path);
 		// segment the file in equal parts among every worker thread
 		const std::streampos segment_size = static_cast<const std::streampos>(std::ceil(file_size / static_cast<double>(threads_count)));
 
 		// frequency_tables[k] is the frequency table 
-		// over which worker[k] is working
+		// over which worker[k] started working
+		// at started_times[k]
 		// TODO: less detached from one another
 		std::vector<Type> frequency_tables(threads_count);
 		std::vector<std::thread> workers(threads_count);
+		std::vector<std::chrono::time_point<std::chrono::steady_clock>> started_times(threads_count);
 
 		// start processing each segment in a new worker thread and in a new frequency table
 		for (unsigned int i = 0; i < threads_count; ++i) {
@@ -92,15 +100,37 @@ namespace FrequencyTable
 			frequency_tables[i].fill(0);
 			assert(frequency_tables.size() <= threads_count);
 
+			started_times[i] = std::chrono::high_resolution_clock::now();
+			assert(started_times.size() <= threads_count);
+
+			if (!is_quiet) {
+				std::lock_guard<std::mutex> lock(_mutex);
+
+				log_stream << "Thread #" << i << " started." << std::endl;
+			}
+
 			workers[i] = std::thread(process, std::ref(file_path), segment_start, segment_end, std::ref(frequency_tables[i]));
 			assert(workers.size() <= threads_count);
 
-			assert(frequency_tables.size() == workers.size());
+			assert(frequency_tables.size() == workers.size() && workers.size() == started_times.size());
 		}
 		// wait all threads to finish 
-		for (std::thread& worker : workers) {
-			if (worker.joinable())
-				worker.join();
+		for (unsigned int i = 0; i < threads_count; ++i) {
+			if (workers[i].joinable())
+			{
+				workers[i].join();
+
+				if (!is_quiet) {
+					std::lock_guard<std::mutex> lock(_mutex);
+
+					log_stream << "Thread #" << i << " stopped." << std::endl;
+
+					auto finish = std::chrono::high_resolution_clock::now();
+					std::chrono::duration<double> elapsed = finish - started_times[i];
+
+					log_stream << "Thread #" << i << " execution time was " << elapsed.count() << "s." << std::endl;
+				}
+			}
 		}
 
 		// accumulate final result
--- main.cpp
+++ main.cpp
@@ -7,31 +7,88 @@
 #include <iostream>
 #include <chrono>
 
+#include <mutex>
+
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
+	std::mutex m;
+
 	auto start = chrono::high_resolution_clock::now();
 
-	string file_path = "D:/dev/c++/src/rsa/huffman-coding/resource-files/lorem-ipsum.txt";
+	std::vector<std::string> arguments;
+	arguments.assign(argv, argv + argc);
+
+	if (!Utils::Options::has_option("-f", arguments)) {
+		std::lock_guard<std::mutex> lock(m);
+
+		std::cerr << "No file specified!" << endl;
+		return -1;
+	}
+	
+	string file_path = Utils::Options::get_option_value("-f", arguments);
 	streampos file_size = Utils::File::get_file_size(file_path);
+	if (file_size == std::streampos(-1)) {
+		std::lock_guard<std::mutex> lock(m);
 
-	unsigned int threads_count = thread::hardware_concurrency();
-	FrequencyTable::Type ft = FrequencyTable::get(file_path, threads_count);
-
-	for (size_t i = 0; i < ft.size() - 1; ++i) {
-		if (ft[i] != 0) {
-			cout << static_cast<char>(i) << ": " << ft[i] << endl;
-		}
+		std::cerr << "File " << file_path << " can not be opened for reading!" << endl;
+		return -2;
 	}
 
-	cout << "Threads count: " << threads_count << endl;
-	cout << "Bytes count: " << ft[256] << endl;
-	cout << "File size: " << file_size << endl;
+	unsigned int threads_count;
+
+	if (!Utils::Options::has_option("-t", arguments)) {
+		std::lock_guard<std::mutex> lock(m);
+
+		std::cerr << "Number of threads not specified!" << endl;
+		std::cerr << "Starting program with maximum threads available!" << endl;
+
+		threads_count = std::thread::hardware_concurrency();
+	}
+
+	try {
+		threads_count = std::stoi(Utils::Options::get_option_value("-t", arguments));
+	}
+	catch (const std::exception& e) {
+		std::lock_guard<std::mutex> lock(m);
+
+		cerr << "Can not parse thread count argument!" << endl;
+		return -3;
+	}
+
+	bool is_quiet = Utils::Options::has_option("-q", arguments);
+
+	FrequencyTable::Type ft;
+	try {
+		ft = FrequencyTable::get(file_path, threads_count, std::cout, is_quiet);
+	}
+	catch (const std::exception& e) {
+		cerr << e.what() << endl;
+		return -4;
+	}
+
+	//for (size_t i = 0; i < ft.size() - 1; ++i) {
+	//	if (ft[i] != 0) {
+	//		cout << static_cast<char>(i) << ": " << ft[i] << endl;
+	//	}
+	//}
+
+	{
+		std::lock_guard<std::mutex> lock(m);
+
+		cout << "Total threads count: " << threads_count << endl;
+		//cout << "Bytes processed count: " << ft[256] << endl;
+		cout << "File size: " << file_size << endl;
+	}
 
 	auto finish = chrono::high_resolution_clock::now();
 	chrono::duration<double> elapsed = finish - start;
-	cout << "Elapsed time: " << elapsed.count() << "s" << endl;
+	{
+		std::lock_guard<std::mutex> lock(m);
+
+		cout << "Total execution time: " << elapsed.count() << "s" << endl;
+	}
 
 	return 0;
 }
